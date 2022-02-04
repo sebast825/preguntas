7b10921c2d6244f4e2bcf531a9be873597c10b06
sebast825 <sebamolina825@gmail.com>
2022-02-04 11:29:11 -0300

    falta terminar fe

--- data/question.js
+++
@@ -1,7 +0,0 @@
-import { Question } from '../models/question.js'
-import {data} from './data.js'
-
-
-export const question = data.map(elem => new Question(elem.question,elem.choices,elem.answer))
-
-
--- models/Question.js
+++ models/Question.js
@@ -1,32 +1,13 @@
-import {data} from '../data/data.js'
-import { question } from '../data/question.js';
+
 
 export class Question {
-
-	/**
-	 * 
-	 * @param {string} question la pregunta 
-	 * @param {string []} choices las opciones 
-	 * @param {string} answer la respuesta
-	 */
-	constructor(question,choices,answer){
+	constructor(question, choices, answer){
 		this.question = question;
 		this.choices = choices;
 		this.answer = answer;
 	}
-	/**
-	 * 
-	 * @param {string} option 
-	 * @returns boolean
-	 */
-	getCorrectAnswer(option){
-		return option === this.answer
+
+	correctAnswer(answer){
+		return answer === this.answer
 	}
 }
-
-const q1 = new Question(data[0].question,data[0].choices,data[0].answer)
-
-
-
-// console.log(q1)
-// console.log(q1.getCorrectAnswer('word'))
